6e60f0aab886c841b48a4432a4562df0bf3e7351
permtvideo <43233238+permtvideo@users.noreply.github.com>
2022-03-27 19:51:06 +0500

    Add files via upload

---
+++ bot_otvet.py
@@ -0,0 +1,21 @@
+import time
+import telebot
+import sqlite3
+from datetime import datetime
+
+bot = telebot.TeleBot('5195821377:AAF8WyEzmyl40dSTcqQp0WVYnvyJjEyUo2o')
+
+while True:
+    if datetime.now().second == 0:
+        conn = sqlite3.connect('bd_bot.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, user_id, user_id_2, join_date, message FROM users")
+        testarray = cursor.fetchall()
+        for i in range(testarray.__len__()):
+            if (int(datetime.now().timestamp()) - testarray[i][3]) >= 60:
+                join_date = int(datetime.now().timestamp())
+                id = testarray[i][0]
+                cursor.execute("UPDATE users SET join_date=(?) WHERE id=(?)", (join_date, id))
+                bot.send_message(chat_id=testarray[i][1], text='https://t.me/' + testarray[i][2] + ' ' + str(testarray[i][0]), reply_to_message_id=testarray[i][0])
+                conn.commit()
+        conn.close()
---
+++ bot_priem.py
@@ -0,0 +1,32 @@
+import time
+import telebot
+import sqlite3
+from datetime import datetime
+
+bot = telebot.TeleBot('5195821377:AAF8WyEzmyl40dSTcqQp0WVYnvyJjEyUo2o')
+
+users = {}
+
+@bot.message_handler()
+def start(message):
+    if datetime.now().second == 0:
+        time.sleep(1)
+    conn = sqlite3.connect('bd_bot.db')
+    cursor = conn.cursor()
+    id = message.message_id
+    user_id = message.chat.id
+    if message.forward_from != None:
+        user_id_2 = message.forward_from.username
+    else:
+        user_id_2 = message.from_user.username
+    join_date = message.date
+    message_id_to_id = message.text
+    if message.reply_to_message != None:
+        id_from_text = message.reply_to_message.text.split()
+        cursor.execute("DELETE from users where id = (?)", (id_from_text[1], ))
+    else:
+        cursor.execute("INSERT INTO users values (?, ?, ?, ?, ?)", (id, user_id, user_id_2, join_date, message_id_to_id, ))
+        bot.reply_to(message, 'запомнил')
+    conn.commit()
+    conn.close()
+bot.polling()
