2060e8848c598d8bf2e8228dedf755194eee0601
permtvideo <43233238+permtvideo@users.noreply.github.com>
2022-03-27 21:22:41 +0500

    Update bot_priem.py

--- bot_priem.py
+++ bot_priem.py
@@ -15,17 +15,18 @@ def start(message):
     cursor = conn.cursor()
     id = message.message_id
     user_id = message.chat.id
+    username = message.chat.username
     if message.forward_from != None:
         user_id_2 = message.forward_from.username
     else:
         user_id_2 = message.from_user.username
     join_date = message.date
     message_id_to_id = message.text
-    if message.reply_to_message != None:
+    if (message.reply_to_message != None):
         id_from_text = message.reply_to_message.text.split()
         cursor.execute("DELETE from users where id = (?)", (id_from_text[1], ))
     else:
-        cursor.execute("INSERT INTO users values (?, ?, ?, ?, ?)", (id, user_id, user_id_2, join_date, message_id_to_id, ))
+        cursor.execute("INSERT INTO users values (?, ?, ?, ?, ?, ?)", (id, user_id, user_id_2, join_date, message_id_to_id,username, ))
         bot.reply_to(message, 'запомнил')
     conn.commit()
     conn.close()
