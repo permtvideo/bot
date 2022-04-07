cb4546d604f788575f6d4020b9d30a89a03ffc12
permtvideo <43233238+permtvideo@users.noreply.github.com>
2022-04-07 23:06:08 +0500

    Update bot_priem.py

--- bot_priem.py
+++ bot_priem.py
@@ -5,29 +5,37 @@ from datetime import datetime
 
 bot = telebot.TeleBot('5195821377:AAF8WyEzmyl40dSTcqQp0WVYnvyJjEyUo2o')
 
-users = {}
-
 @bot.message_handler()
 def start(message):
-    if datetime.now().second == 0:
-        time.sleep(1)
-    conn = sqlite3.connect('bd_bot.db')
-    cursor = conn.cursor()
-    id = message.message_id
-    user_id = message.chat.id
-    username = message.chat.username
-    if message.forward_from != None:
-        user_id_2 = message.forward_from.username
+    if message.text == '/start' or message.text == '/help':
+        bot.send_message(chat_id=message.chat.id, text='Привет! Я бот-напоминалка. Любое отправленное мне сообщение будет запомнено и возвращено Вам через полтора часа. Если необходимо напомнить через другое время, то надо ответить на мое сообщение, в котором содержится ссылка на запомненную информацию, числом (можно использовать дробное число, например 0,5) и указать время в часах, начиная от текущего момента. После срабатывания напоминания, оно перейдет в режим ожидания на следующие полтора часа, время снова можно изменить, ответив на мое сообщение. Также можно запоминать пересланные из других чатов сообщения, тогда я напомню вам в каком чате требуется ваш ответ. Чтобы удалить напоминание, ответьте мне любым символом, кроме числа. Для вызова справки используйте команду /start или /help.')
     else:
-        user_id_2 = message.from_user.username
-    join_date = message.date
-    message_id_to_id = message.text
-    if (message.reply_to_message != None):
-        id_from_text = message.reply_to_message.text.split()
-        cursor.execute("DELETE from users where id = (?)", (id_from_text[1], ))
-    else:
-        cursor.execute("INSERT INTO users values (?, ?, ?, ?, ?, ?)", (id, user_id, user_id_2, join_date, message_id_to_id,username, ))
-        bot.reply_to(message, 'запомнил')
-    conn.commit()
-    conn.close()
+        if datetime.now().second == 0:
+            time.sleep(1)
+        conn = sqlite3.connect('bd_bot.db')
+        cursor = conn.cursor()
+        id = message.message_id
+        user_id = message.chat.id
+        username = message.chat.username
+        if message.forward_from != None:
+            user_id_2 = message.forward_from.username
+        else:
+            user_id_2 = message.from_user.username
+        join_date = message.date
+        message_id_to_id = message.text
+        if (message.reply_to_message != None):
+            proverka = message.text.replace(",", ".")
+            id_from_text = message.reply_to_message.text.split()
+            try:
+                float(proverka)
+                join_date = int(message.date - 5400 + float(proverka) * 3600)
+                cursor.execute("UPDATE users SET join_date=(?) WHERE id=(?)", (join_date, id_from_text[0]))
+            except ValueError:
+                cursor.execute("DELETE from users where id = (?)", (id_from_text[0], ))
+                bot.reply_to(message, str(id) + ' Напоминание было удалено')
+        else:
+            cursor.execute("INSERT INTO users values (?, ?, ?, ?, ?, ?)", (id, user_id, user_id_2, join_date, message_id_to_id,username, ))
+            bot.reply_to(message, str(id) + ' Ответ цифрой задает время напоминания (в часах), другим символом отменяет его')
+        conn.commit()
+        conn.close()
 bot.polling()
