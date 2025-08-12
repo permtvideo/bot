01bf560de4ceb1f4b979af56a1cf2d5909c3efdd
permtvideo <43233238+permtvideo@users.noreply.github.com>
2025-08-12 23:03:11 +0500

    Update bot_otvet.py

--- bot_otvet.py
+++ bot_otvet.py
@@ -34,7 +34,7 @@ def cikl():
             conn.commit()
             bot.send_message(chat_id=306095025, text=str(testarray[i][0]) + ' ' + str(testarray[i][2]) + '  удалено из-за блока юзером')
         else:
-            if " message to be replied not found " in str(e):
+            if "message to be replied not found" in str(e):
                 cursor.execute("UPDATE users SET join_date=(?) WHERE id=(?)", (join_date, id))
                 conn.commit()
                 bot.send_message(chat_id=testarray[i][1], text=str(testarray[i][0]) + ' ' + str(testarray[i][4]))
@@ -45,6 +45,7 @@ def cikl():
         print(e)
         cikl()
 cikl()
+
 
 
 
